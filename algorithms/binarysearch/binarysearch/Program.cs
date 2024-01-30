077fdfee02e144fb965cb56c33086357d1bd8785
BoonyToons <55165929+BoonyToons@users.noreply.github.com>
2024-01-29 18:31:10 -0600

    binary and linear search

---
+++ algorithms/binarysearch/binarysearch/Program.cs
@@ -0,0 +1,31 @@
+﻿
+class BinarySearch {
+    static void Main(string[] args) {
+
+    }
+
+    static void Binary(int[] numbers, int num) {
+        
+        int len = 0;
+        if(numbers.Length % 2 == 0) {
+            len = numbers.Length / 2;
+        }
+        else {
+            len = (numbers.Length - 1)/2;
+        }
+        
+        bool trigger = true;
+        while(trigger) {
+            if(num == numbers[len]) {
+                trigger = false;
+                break;
+            }
+            else if (num < numbers[len/2]) {
+                len /= 2;
+            }
+            else if (num > numbers[len/2]) {
+                len += (len/2);
+            }
+        }
+    }
+}
---
+++ algorithms/linearsearch/linearsearch/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Configuration.Assemblies;
+
+class LinearSearch {
+    static void Main(string[] args) {
+        Linear([1,5,2,7,8,2], 5);
+        Linear([1,5,2,7,8,2], 8);
+        Linear([1,5,2,7,8,2], 1);
+        Linear([1,5,2,7,8,2], 0);
+    }
+
+    static void Linear(int[] numbers, int num) {
+        for(int ii=0; ii<numbers.Length; ii++) {
+            if(num == numbers[ii]) {
+                Console.WriteLine(num);
+                break;
+            }
+        }
+    }
+}
