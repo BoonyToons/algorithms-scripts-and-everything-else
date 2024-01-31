4d47eba00745da738b2bd842481ca53a65f1b64f
BoonyToons <55165929+BoonyToons@users.noreply.github.com>
2024-01-30 18:32:19 -0600

    binary search WIP

--- algorithms/binarysearch/binarysearch/Program.cs
+++ algorithms/binarysearch/binarysearch/Program.cs
@@ -1,31 +1,35 @@
 ﻿
 class BinarySearch {
     static void Main(string[] args) {
-
+        Binary([-6,-6,-2,-1,0,1,2,3,4,5,6,7,8,8,8,9,9,10,11], 2);
+        Binary([-6,-6,-2,-1,0,1,2,3,4,5,6,7,8,8,8,9,9,10,11], 5);
+        Binary([-6,-6,-2,-1,0,1,2,3,4,5,6,7,8,8,8,9,9,10,11], 8);
+        Binary([-6,-6,-2,-1,0,1,2,3,4,5,6,7,8,8,8,9,9,10,11], 11);
+        Binary([-6,-6,-2,-1,0,1,2,3,4,5,6,7,8,8,8,9,9,10,11], -2);
+        Binary([-6,-6,-2,-1,0,1,2,3,4,5,6,7,8,8,8,9,9,10,11], -6);
+        Binary([-6,-6,-2,-1,0,1,2,3,4,5,6,7,8,8,8,9,9,10,11], 20);    
     }
 
     static void Binary(int[] numbers, int num) {
-        
-        int len = 0;
-        if(numbers.Length % 2 == 0) {
-            len = numbers.Length / 2;
-        }
-        else {
-            len = (numbers.Length - 1)/2;
-        }
-        
-        bool trigger = true;
-        while(trigger) {
-            if(num == numbers[len]) {
-                trigger = false;
-                break;
+        int high = numbers.Length - 1;
+        int low = 0;
+        int middle;
+
+        do {
+            middle = low + (high - low) / 2;
+            if(num == numbers[middle]) {
+                Console.WriteLine(num);
             }
-            else if (num < numbers[len/2]) {
-                len /= 2;
+            else if(num > numbers[middle]) {
+                low = middle;
             }
-            else if (num > numbers[len/2]) {
-                len += (len/2);
+            else if(num < numbers[middle]) {
+                high = middle;
             }
-        }
+            else {
+                Console.WriteLine("Your item was not found.");
+            }
+
+        } while(low < high);
     }
 }
