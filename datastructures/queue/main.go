fb06c1f73920bb4bcb6654065d23eecc19d68e8c
Oliver <christof.cline@g.austincc.edu>
2024-02-28 00:56:27 -0600

    implement queue functions
    note: the queue is backwards and dequeue is not possible due to this. make sure the head is actually the current tail value instead.
    
    current:  e -> d -> c -> b -> a
    should be: a -> b -> c -> d -> e
    
    a is the head

--- datastructures/queue/main.go
+++ datastructures/queue/main.go
@@ -4,25 +4,75 @@ import (
 	"fmt"
 )
 
-type node[T any] struct {
-	value T
-	next *node[T]
+type node struct {
+	value interface{}
+	next *node
 }
 
-type Queue[T any] struct {
+type Queue struct {
 	length int
-	head *node[T]
-	tail *node[T]
+	head *node
+	tail *node
 }
 
 type queueInterface interface {
 	enqueue()
 	dequeue()
 	peek()
+	printQueue()
+}
+
+func (queue *Queue) enqueue(val interface{}) {
+	newNode := &node{value: val}
+	if queue.length == 0 {
+		queue.head = newNode 
+		queue.tail = newNode
+	} else {
+		newNode.next = queue.tail
+		queue.tail = newNode
+	}
+	queue.length++
+}
+
+func (queue *Queue) dequeue() interface{} {
+	returnValue := "There are not enough elements in this queue to remove"
+	if queue.length > 0 {
+		returnValue = queue.head.value
+		
+		queue.length--
+	}
+	return returnValue
+}
+
+func (queue *Queue) peek() interface{} {
+	return queue.head.value
 }
  
+func (queue *Queue) printQueue() {
+	printValue := queue.tail
+	if queue.length > 1 {
+		for i:=0; i<queue.length; i++ {
+			fmt.Printf("%v ", printValue.value)
+			printValue = printValue.next
+		}
+		fmt.Println("")
+	} else if queue.length == 1 {
+		fmt.Println("%v ", printValue.value)
+	} else {
+		fmt.Println("There are no elements in this queue to print")
+	}
+	
+}
+
 
 func main() {
 	fmt.Println("---- This is a Queue Data Structure ----")
+	newQueue := Queue{}
+	newQueue.enqueue(15)
+	newQueue.enqueue(33)
+	newQueue.enqueue("Hello there")
+	newQueue.enqueue(true)
 
+	fmt.Println(newQueue.peek())
+	newQueue.printQueue()
 }
