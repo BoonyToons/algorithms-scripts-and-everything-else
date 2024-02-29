f6bf9150548235acda02ee822233aa1482e7ec48
Oliver <christof.cline@g.austincc.edu>
2024-02-29 00:52:34 -0600

    create the node, stack struct and interface with methods

--- datastructures/stack/main.go
+++ datastructures/stack/main.go
@@ -4,6 +4,30 @@ import (
 	"fmt"
 )
 
+type node struct {
+	value interface{}
+	prev *node
+}
+
+type Stack struct {
+	length int
+	head *node
+	tail *node
+}
+
+type stackInterface interface {
+	push()
+	pop()
+	peek()
+	getLength()
+	isEmpty()
+	printStack()
+}
+
+func (stack *Stack) push() {
+
+}
+
 func main() {
 	fmt.Println("---- This is a Stack Data Structure ----")
 }
