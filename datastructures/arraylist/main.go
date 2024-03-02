d4327195189523dc944fd372881b806cf353d5ac
BoonyToons <55165929+BoonyToons@users.noreply.github.com>
2024-03-01 18:32:21 -0600

    initial commit for arraylist

---
+++ datastructures/arraylist/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ArrayList[T type] struct {
+	itemLength int
+	arrayLength int
+	newArray []T
+
+}
+
+type arrayListInterface interface {
+
+}
+
+func main() {
+	fmt.Println("---- This is an ArrayList Data Structure ----")
+}
