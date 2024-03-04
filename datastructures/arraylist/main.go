ac86a4b0097068b0c00d9715f43d577464bf622d
Oliver <christof.cline@g.austincc.edu>
2024-03-04 00:19:07 -0600

    wip

--- datastructures/arraylist/main.go
+++ datastructures/arraylist/main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 )
 
-type ArrayList[T type] struct {
+type ArrayList[T any] struct {
 	itemLength int
 	arrayLength int
 	newArray []T
-
 }
 
 type arrayListInterface interface {
-
+	NewArrayList()
 }
+
+func (list *ArrayList) NewArrayList(initialSize int) *ArrayList {
+	return &list{
+		itemLength: 0
+		arrayLength: initialSize
+		newArray: []
+	}
+}
+
+
 
 func main() {
 	fmt.Println("---- This is an ArrayList Data Structure ----")
