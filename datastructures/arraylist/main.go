7fdefded80fe225ea5b5f5648223d5cd096bbeb2
BoonyToons <55165929+BoonyToons@users.noreply.github.com>
2024-03-05 18:33:54 -0600

    working with error handling for intantiation

--- datastructures/arraylist/main.go
+++ datastructures/arraylist/main.go
@@ -10,23 +10,49 @@ type ArrayList[T any] struct {
 	newArray []T
 }
 
-func NewArrayList[T any](arrayLength int) *ArrayList[T] {
-	return &ArrayList[T] {
+func NewArrayList[T any](arrayLength int) (*ArrayList[T], error) {
+	returnValue := &ArrayList[T] {
 		itemLength: 0,
 		arrayLength: arrayLength,
 		newArray: make([]T, arrayLength),
 	}
+
+	var err error
+
+	if arrayLength < 1 {
+		returnValue = nil
+		err = "Hello"
+	}
+
+	return returnValue, err
 }
 
 type arrayListInterface interface {
-	push()
+	add()
+	insert()
+	get()
+	set()
+	remove()
+	contains()
+	indexOf()
+	length()
+	isEmpty()
+	clear()
 }
 
+
+func (list *ArrayList[T]) add() {
+	fmt.Println("Hello")
+}
 
 
 func main() {
 	fmt.Println("---- This is an ArrayList Data Structure ----")
 
-	list := NewArrayList[int](6)
-	fmt.Println(list)
+	list, err := NewArrayList[int](6)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	list.add()
 }
